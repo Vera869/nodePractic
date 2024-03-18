60e12bfaa37bd227462c31b5d7d77001e17a41c0
Vera869 <verea8910@gmail.com>
2024-03-18 12:28:00 +0700

    bugg env

--- src/app.js
+++ src/app.js
@@ -1,25 +1,46 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const getUsers = require('./modules/users');
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const getUsers = require('./modules/users');
 
-const server = http.createServer((request, response) => {
-   if(request.url === '/users') {
-      response.status = 200;
-      response.statusMessange = "ok";
-      response.header = "Content-Type: application/json";
-      response.write(getUsers());
-      response.end();
+// const server = http.createServer((request, response) => {
+//    if(request.url === '/users') {
+//       response.status = 200;
+//       response.statusMessange = "ok";
+//       response.header = "Content-Type: application/json";
+//       response.write(getUsers());
+//       response.end();
 
-      return;
-   }
-   response.status = 200;
-   response.statusMessange = "ok";
-   response.header = "Content-Type: text/plain";
-   response.write("Hello Everyone!!!");
-   response.end();
-});
+//       return;
+//    }
+//    response.status = 200;
+//    response.statusMessange = "ok";
+//    response.header = "Content-Type: text/plain";
+//    response.write("Hello Everyone!!!");
+//    response.end();
+// });
 
-server.listen(port, hostname, () => {
-   console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
+// server.listen(port, hostname, () => {
+//    console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
+// }) PORT=3005 
+
+const express = require('express');
+
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const {
+   PORT = 3003,
+   API_URL = "127.0.0.1"
+} = process.env;
+
+app.get("/", (request, response) => {
+   response.status(200);
+   response.send("Hello Everyone!!!");
 })
+
+app.listen(PORT, () => {
+      console.log(`Сервер запущен по адресу http://${API_URL}:${PORT}/`);
+    })
